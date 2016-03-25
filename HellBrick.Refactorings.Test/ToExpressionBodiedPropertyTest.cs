7b278896b4ade136ead91cc3a0fdc02ab7da485b
HellBrick <hellbrick@outlook.com>
2016-03-25 22:42:21 +0300

    Added tests for expression bodied member refactorings

---
+++ HellBrick.Refactorings.Test/ToExpressionBodiedPropertyTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HellBrick.Refactorings.ExpressionBodies;
+using Xunit;
+
+namespace HellBrick.Refactorings.Test
+{
+	public class ToExpressionBodiedPropertyTest
+	{
+		private ToExpressionBodiedPropertyRefactoring _provider = new ToExpressionBodiedPropertyRefactoring();
+
+		[Fact]
+		public void MultipleLinePropertyMethodIsIgnored()
+		{
+			const string sourceCode = "int Value {	get { int x = 2; return x * x; } }";
+			_provider.ShouldNotProvideRefactoring( sourceCode );
+		}
+
+		[Fact]
+		public void PropertyWithSetterIsIgnored()
+		{
+			const string sourceCode = "int Value {	get { return 42; } set {} }";
+			_provider.ShouldNotProvideRefactoring( sourceCode );
+		}
+
+		[Fact]
+		public void SingleLineThrowingPropertyIsIgnored()
+		{
+			const string sourceCode = "int Value { get { throw new Exception(); } }";
+			_provider.ShouldNotProvideRefactoring( sourceCode );
+		}
+
+		[Fact]
+		public void ExpressionBodiedPropertyIsIgnored()
+		{
+			const string sourceCode = "int Calc => 42;";
+			_provider.ShouldNotProvideRefactoring( sourceCode );
+		}
+
+		[Fact]
+		public void SingleLineReturningPropertyIsConverted()
+		{
+			const string sourceCode = "int Value { get { return 42; } }\r\n";
+			const string expected = "int Value => 42;\r\n";
+			_provider.ShouldProvideRefactoring( sourceCode, expected );
+		}
+	}
+}
