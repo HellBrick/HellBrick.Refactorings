3ac83e662da2464efa4ff185715ada7b2be2a752
HellBrick <hellbrick@outlook.com>
2016-03-27 10:15:00 +0300

    Changed dll name

--- HellBrick.Refactorings.Vsix/Properties/AssemblyInfo.cs
+++ HellBrick.Refactorings.Vsix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "HellBrick.Diagnostics.Refactorings.Vsix" )]
+[assembly: AssemblyTitle( "HellBrick.Refactorings.Vsix" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "HellBrick.Diagnostics.Refactorings.Vsix" )]
+[assembly: AssemblyProduct( "HellBrick.Refactorings.Vsix" )]
 [assembly: AssemblyCopyright( "" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
